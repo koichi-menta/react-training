ec94df51d6bca9f045369c58e26b916ffd4c2bd0
koichi-menta <101hundredone@gmail.com>
2021-12-10 15:59:25 +0900

    feat: todoの更新削除を追加

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,4 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const TodoListItem = ({ index, item, todo, setTodo }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [name, setName] = useState(item);
+
+  const handleEdit = () => setIsEdit(true);
+
+  const handleUpdate = () => {
+    todo[index] = name;
+    setTodo([...todo]);
+    setIsEdit(false);
+  };
+
+  const handleDelete = () => {
+    todo.splice(index, 1);
+    setTodo([...todo]);
+  };
+
+  useEffect(() => {
+    setName(item);
+  }, [item]);
+
+  return (
+    <li>
+      {isEdit ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button onClick={handleUpdate}>更新</button>
+        </>
+      ) : (
+        <>
+          {item}
+          <button onClick={handleEdit}>編集</button>
+        </>
+      )}
+      <button onClick={handleDelete}>削除</button>
+    </li>
+  );
+};
 
 const App = () => {
   const [todo, setTodo] = useState([]);
@@ -22,8 +65,16 @@ const App = () => {
       />
       <button onClick={handleAddTodo}>登録</button>
       <ul>
-        {todo.map((item) => {
-          return <li>{item}</li>;
+        {todo.map((item, index) => {
+          return (
+            <TodoListItem
+              index={index}
+              item={item}
+              // TODO: todoを書き換える為に全部渡しているが無駄なのでリファクタできる
+              todo={todo}
+              setTodo={setTodo}
+            />
+          );
         })}
       </ul>
     </div>
